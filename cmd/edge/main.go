2be4d502ab6989bcc4364e12bd66225d39cb8f3d
ruba <rubaosman10@gmail.com>
2025-11-28 20:38:05 -0600

    Modularize TCP server

--- cmd/edge/main.go
+++ cmd/edge/main.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"cdn-edge-server/internal/server"
-	"fmt"
-	"net"
 )
 
 const (
@@ -15,27 +13,7 @@ const (
 func main() {
 	// logic to run terminal CLI (or GUI?)
 
-	// Start socket listener
-	listener, err := net.Listen("tcp", HOST+":"+PORT)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer listener.Close()
-
-	fmt.Println("Listening on", HOST+":"+PORT)
-
-	// Listen for incoming connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			// Error occurred while accepting client connection, skip
-			fmt.Println("Accept error:", err)
-			continue
-		}
-
-		fmt.Println("Client connected from:", conn.RemoteAddr())
-
-		go server.HandleClient(conn) // concurrently handle clients
-	}
+	// Start TCP server and serve clients
+	srv := server.NewTCPServer(HOST, PORT, server.HandleClient)
+	srv.ListenAndServe()
 }
--- internal/server/tcp_server.go
+++ internal/server/tcp_server.go
@@ -1,1 +1,45 @@
 package server
+
+import (
+	"fmt"
+	"net"
+)
+
+type TCPServer struct {
+	Host    string
+	Port    string
+	Handler func(net.Conn)
+}
+
+func NewTCPServer(host, port string, handler func(net.Conn)) *TCPServer {
+	return &TCPServer{
+		Host:    host,
+		Port:    port,
+		Handler: handler,
+	}
+}
+
+func (s *TCPServer) ListenAndServe() error {
+	// Start socket listener
+	addr := s.Host + ":" + s.Port
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("listen error: %w", err)
+	}
+
+	fmt.Println("Server listening on", addr)
+
+	// Listen for incoming client connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// Error occurred while accepting client connection, skip that client and keep listening
+			fmt.Println("accept error:", err)
+			continue
+		}
+		fmt.Println("Client connected from:", conn.RemoteAddr())
+
+		// Concurrently handle client connections
+		go HandleClient(conn)
+	}
+}
